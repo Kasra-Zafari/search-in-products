608f07e87f28ea7ff4f2fc0f50e0a8d1c61f98ec
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-06 20:52:29 +0330

    finished header

--- src/componets/Header/index.jsx
+++ src/componets/Header/index.jsx
@@ -3,16 +3,22 @@ import React, { useEffect, useState } from "react"
 
 const Header = () => {
 
-const [categories, setCategories] = useState([]);
-const [isLoading,setIsLoading] = useState(true);
+    const [categories, setCategories] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedCategory, setSelectedCategory] = useState("")
 
-useEffect(()=>{
-    fetch('https://fakestoreapi.com/products/categories')
-    .then((res)=>res.json())
-    .then((data)=>{setCategories(data)})
-    .catch((error)=>{alert("an error has been occured")})
-    .finally(()=>{setIsLoading(false)})
-},[])
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/categories')
+            .then((res) => res.json())
+            .then((data) => { setCategories(data) })
+            .catch((error) => { alert("an error has been occured") })
+            .finally(() => { setIsLoading(false) })
+    }, [])
+
+
+    const handleSelectCategory = (item) => {
+        setSelectedCategory(item);
+    }
 
 
     return (
@@ -21,8 +27,8 @@ useEffect(()=>{
             <nav className={classes.nav}>
                 {isLoading && <p>Category Is Loading...</p>}
 
-                {categories.map((category)=>(
-                    <button key={category}>{category}</button>
+                {categories.map((category) => (
+                    <button key={category} onClick={() => handleSelectCategory(category)}>{category}</button>
                 ))}
             </nav>
             <div>
