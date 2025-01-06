44433631ee86d648b521b034c1b000920e6d436a
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-06 20:36:17 +0330

    api header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Header from './componets/Header'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>hello</h1>
-      </div>
+      <Header/>
     </>
   )
 }
---
+++ src/componets/Header/index.jsx
@@ -0,0 +1,35 @@
+import classes from "./index.module.css"
+import React, { useEffect, useState } from "react"
+
+const Header = () => {
+
+const [categories, setCategories] = useState([]);
+const [isLoading,setIsLoading] = useState(true);
+
+useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/categories')
+    .then((res)=>res.json())
+    .then((data)=>{setCategories(data)})
+    .catch((error)=>{alert("an error has been occured")})
+    .finally(()=>{setIsLoading(false)})
+},[])
+
+
+    return (
+        <header className={classes.header}>
+            <a href="#" className={classes.logo}><img src="logo.png" alt="logo" width="100%" /></a>
+            <nav className={classes.nav}>
+                {isLoading && <p>Category Is Loading...</p>}
+
+                {categories.map((category)=>(
+                    <button key={category}>{category}</button>
+                ))}
+            </nav>
+            <div>
+                <button>Login</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
